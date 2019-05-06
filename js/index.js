240a8a3d60c2dfaa6741a3c6f8c5e8445a4b43f6
Nikki A <nikki.apostolakis@gmail.com>
2019-05-06 23:14:53 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -3,12 +3,12 @@ function getRepositories()
 {
   let username = document.getElementById('username').value;
   const req = new XMLHttpRequest();
-  req.addEventListener('load', showRepositories);
+  req.addEventListener('load', displayRepositories);
   req.open('GET', 'https://api.github.com/users/'+username+'/repos');
   req.send();
 
 }
-function showRepositories()
+function displayRepositories()
 {
   var repos = JSON.parse(this.responseText);
   console.log(repos);
