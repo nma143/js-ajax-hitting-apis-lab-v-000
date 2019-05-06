5dd1e30168c927379dc563c92b34ec910a67cf05
Nikki A <nikki.apostolakis@gmail.com>
2019-05-06 23:06:52 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -30,7 +30,7 @@ function showRepositories()
 function getCommits(el)
 {
   const name = el.dataset.repo;
-  const username = document.getElementById('username').value;
+  const username = el.dataset.username;
 
   const req = new XMLHttpRequest();
   req.addEventListener('load', displayCommits);
